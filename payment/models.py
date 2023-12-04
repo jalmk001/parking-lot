ffea8572d71664c2020e49e40af1c7abe0145d99
ramshad-itday <md.ramshad@itday.in>
2023-12-04 23:58:14 +0530

    changes: payment model

--- payment/models.py
+++ payment/models.py
@@ -3,7 +3,10 @@ from park.models import Park
 from user.models import User
 # Create your models here.
 
-
+PAYMENT_STATUSES = (
+    ('pending', 'pending'),
+    ('confirmed', 'confirmed')
+)
 class Payment(models.Model):
     py_id = models.AutoField(primary_key=True)
     s=models.ForeignKey(Park,to_field='s_id',on_delete=models.CASCADE)
@@ -12,8 +15,8 @@ class Payment(models.Model):
     # u_id = models.IntegerField()
     date = models.DateField()
     time = models.TimeField()
-    # amount = models.CharField(max_length=100)
-    # status = models.CharField(max_length=100)
+    amount = models.CharField(default='200', max_length=100)
+    status = models.CharField(default='pending', choices=PAYMENT_STATUSES, max_length=100)
 
 
     # class Meta:
